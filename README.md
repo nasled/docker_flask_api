d236dee8a29d5ff6c6b50f3ad0e6a5f5aa348b7a
nasled <naslednique@ya.ru>
2019-04-24 04:19:09 -0400

    implemented a car schema document

--- README.md
+++ README.md
@@ -1,47 +1,93 @@
 # docker_flask_api
-Example of Flask API application wrapped in Docker
+RESTful API Web Service Implementation based on Flask wrapped in Docker 
 
+This service saves and outputs Car Document to MongoDB.
+Document example:
+```
+{
+    title: "Jeep Wrangler Sahara",
+    drive_type: "4wd",
+    transmission: "automatic",
+    engine_size: 3.6,
+    year: 2018,
+    cylinders: 6,
+    width: 73.7,
+    height: 72.6,
+    weight: 4055
+}
+```
 
-## install dependencies
+## How to Deploy
+
+### Install dependencies
+```
 sudo pacman -S docker docker-compose
+```
 
-# build containers 
+### Build containers 
+```
 sudo docker-compose build
+```
 
-# run in background
-sudo docker-compose run -d
+### Run in background
+```
+sudo docker-compose up -d
+```
 
+## How to Use
 
-
-# build image
-sudo docker build -t art/flask .
-
-# run container
-sudo docker run -d -p 5000:5000 --name flaskc art/flask
-
-# remove stopped containers
-sudo docker container prune
-
-# remove stopped images
-sudo docker image prune
-
-# remove unused data
-sudo docker system prune
-
-# remove all images
-sudo docker rmi $(sudo docker images -aq)
-
-# view container logs
-sudo docker logs flaskc
-
-# shell into the container
-sudo docker exec -ti flaskc /bin/bash
-
-    
-
-# test from host or docker container
+### Test from host or docker container
+```
 curl -XGET localhost:5000
-curl -XPOST http://localhost:5000/save -d '{"hello": "world"}'
+curl -XPOST http://localhost:5000/save -d '{"title": "Jeep Wrangler Sahara", "drive_type": "4wd", "transmission": "automatic", "engine_size": 3.6, "year": 2018, "cylinders": 6, "width": 73.7, "height": 72.6, "weight": 4055}'
+curl -XPOST http://localhost:5000/save -d '{"title": "Dodge Ram", "drive_type": "4wd", "transmission": "automatic", "engine_size": 3.6, "year": 2016, "cylinders": 6, "width": 79, "height": 75, "weight": 5226}'
+curl -XPOST http://localhost:5000/save -d '{"title": "Honda Accord EX-L", "drive_type": "2wd", "transmission": "automatic", "engine_size": 3.5, "year": 2016, "cylinders": 6, "width": 73, "height": 56, "weight": 3528}'
+curl -XPOST http://localhost:5000/save -d '{"title": "2015 Hyundai Sonata", "drive_type": "2wd", "transmission": "automatic", "engine_size": 2.4, "year": 2016, "cylinders": 4, "width": 73, "height": 58, "weight": 3245}'
 curl -XGET http://localhost:5000/list
+curl -XGET http://localhost:5000/get/5cc016f93b80a16ee15ffd36
+```
+
+## Usefull Commands
+
+### Build image
+```
+sudo docker build -t art/flask .
+```
+
+### Run container
+```
+sudo docker run -d -p 5000:5000 --name flaskc art/flask
+```
+
+### Remove stopped containers
+```
+sudo docker container prune
+```
+
+### Remove stopped images
+```
+sudo docker image prune
+```
+
+### Remove unused data
+```
+sudo docker system prune
+```
+
+### Remove all images
+```
+sudo docker rmi $(sudo docker images -aq)
+```
+
+### View container logs
+```
+sudo docker logs flaskc
+```
+
+### Shell into the container
+```
+sudo docker exec -ti flaskc /bin/bash
+```
+    
 
 
--- api.py
+++ api.py
@@ -6,6 +6,7 @@ import os
 
 app = Flask(__name__)
 app.config['MONGOALCHEMY_SERVER'] = os.environ.get('DOCKER_MONGO_HOSTNAME')
+# app.config['MONGOALCHEMY_SERVER'] = "localhost"
 app.config['MONGOALCHEMY_DATABASE'] = "mongodb"
 
 api = Api(app)
@@ -15,41 +16,114 @@ db = MongoAlchemy(app)
 '''
 Document Structure
 '''
-class Book(db.Document):
-    name = db.StringField()
+
+
+class Car (db.Document):
     title = db.StringField()
-    year = db.IntField()
+    drive_type = db.StringField()
+    transmission = db.StringField()
+    engine_size = db.FloatField(min_value=0)
+    year = db.IntField(min_value=0)
+    cylinders = db.IntField(min_value=0)
+    width = db.FloatField(min_value=0)
+    height = db.FloatField(min_value=0)
+    weight = db.IntField(min_value=0)
+
 
 '''
 Endpoints
 '''
+
+
 class ApiIndexCall(Resource):
     def get(self):
-        endoints = {}
-        endoints['/list'] = 'latest data list'
-        endoints['/save'] = 'save data to the endpoint'
-        endoints['/get/<string:id>'] = 'get unique entry by id'
-        return jsonify(enpoints=endoints)
+        return jsonify(enpoints={
+            '/list': 'display a collection of records',
+            '/get/<string:id>': 'display a single record that the corresponds to an ID',
+            '/save': 'create a record'
+        })
+
 
 class ApiListCall(Resource):
     def get(self):
-        res = []
-        data = Book.query.all()
-        for d in data:
-           res.append(d.title)
-        return jsonify(data=res)
+        result = {}
+        try:
+            cars = Car.query.all()
+            list = []
+            for car in cars:
+                list.append({
+                    'mongo_id': str(car.mongo_id),
+                    'title': car.title,
+                    'drive_type': car.drive_type,
+                    'transmission': car.transmission,
+                    'engine_size': car.engine_size,
+                    'year': car.year,
+                    'cylinders': car.cylinders,
+                    'width': car.width,
+                    'height': car.height,
+                    'weight': car.weight
+                })
+            result['data'] = list
+            result['error'] = False
+
+        except Exception as e:
+            result['description'] = str(e)
+            result['error'] = True
+
+        return jsonify(result)
+
 
 class ApiGetCall(Resource):
     def get(self, id):
-        data = Book.query.filter(Book.year == int(id)).first()
-        return jsonify(data=data.title)
+        result = {}
+        try:
+            car = Car.query.filter(Car.mongo_id == str(id)).first()
+            result['data'] = {
+                'mongo_id': str(car.mongo_id),
+                'title': car.title,
+                'drive_type': car.drive_type,
+                'transmission': car.transmission,
+                'engine_size': car.engine_size,
+                'year': car.year,
+                'cylinders': car.cylinders,
+                'width': car.width,
+                'height': car.height,
+                'weight': car.weight
+            }
+            result['error'] = False
+
+        except Exception as e:
+            result['description'] = str(e)
+            result['error'] = True
+
+        return jsonify(result)
+
 
 class ApiSaveCall(Resource):
     def post(self):
         json_data = request.get_json(force=True)
-        dive = Book(title='Dive Into Python', year=2004, name='Mark Pilgrim')
-        dive.save()
-        return jsonify(data=json_data)
+        result = {}
+
+        try:
+            model = Car(title=json_data['title'],
+                        drive_type=json_data['drive_type'],
+                        transmission=json_data['transmission'],
+                        engine_size=json_data['engine_size'],
+                        year=json_data['year'],
+                        cylinders=json_data['cylinders'],
+                        width=json_data['width'],
+                        height=json_data['height'],
+                        weight=json_data['weight'])
+            model.save()
+            result['mongo_id'] = str(model.mongo_id)
+            result['error'] = False
+
+        except Exception as e:
+            result['description'] = str(e)
+            result['error'] = True
+
+        return jsonify(result)
+
 
 '''
 Routes
